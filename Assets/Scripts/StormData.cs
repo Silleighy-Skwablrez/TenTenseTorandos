d80a2e29f5183d9d960e4ba2e143234e7fc98733
BobbyStrangler <146020271+xvsd@users.noreply.github.com>
2025-05-07 20:21:17 -0700

    Update StormData.cs
    forgot this

--- Assets/Scripts/StormData.cs
+++ Assets/Scripts/StormData.cs
@@ -1,49 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class StormData : MonoBehaviour
+[Serializable]
+public class StormData
 {
-    public double flooding = 0; // Saturates wood, damages low-elevation electronics, and can collapse structures at high levels.
-    public double lightning = 0; // Starts fires and can damage high-elevation electronics.
-    public double wind = 0; // Can forcibly drop premature coconuts from trees, damage structures, and (possibly) damage thin and tall structures more severely
-    public double electrical = 0; // This is more of a fantasy metric, an EMP or electromagnetic storm can damage electronics.
+    [Header("Storm Metrics")]
+    [Tooltip("Causes water damage, saturates wood, damages low-elevation electronics")]
+    [Range(0, 10)]
+    public float flooding = 0;
+    
+    [Tooltip("Starts fires and damages high-elevation electronics")]
+    [Range(0, 10)]
+    public float lightning = 0;
+    
+    [Tooltip("Damages structures based on height and size")]
+    [Range(0, 10)]
+    public float wind = 0;
+    
+    [Tooltip("EMP-like effect that damages electronics")]
+    [Range(0, 10)]
+    public float electrical = 0;
+    
+    [Header("Storm Properties")]
+    [Tooltip("Direction in degrees (0-359)")]
+    [Range(0, 359)]
     public int direction = 0;
-    public int airPressure = 0;
-    // Start is called before the first frame update
-    void Start()
+    
+    [Tooltip("Air pressure in hPa")]
+    [Range(900, 1100)]
+    public int airPressure = 1013; // Standard atmospheric pressure
+    
+    [Tooltip("Name of the storm")]
+    public string stormName = "Unnamed Storm";
+    
+    [Tooltip("Category of the storm (1-5)")]
+    [Range(1, 5)]
+    public int category = 1;
+    
+    public float GetFloodingMetric() => flooding;
+    public float GetLightningMetric() => lightning;
+    public float GetWindMetric() => wind;
+    public float GetElectricalMetric() => electrical;
+    public int GetDirection() => direction;
+    public int GetAirPressure() => airPressure;
+    public string GetStormName() => stormName;
+    public int GetCategory() => category;
+    
+    public float GetOverallIntensity()
     {
-
+        return (flooding + lightning + wind + electrical) / 4f;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public StormData Clone()
     {
-
-    }
-
-    public int getDirection()
-    {
-        return direction;
-    }
-
-    public double getFloodingMetric()
-    {
-        return flooding;
-    }
-
-    public double getLightningMetric()
-    {
-        return lightning;
-    }
-
-    public double getWindMetric()
-    {
-        return wind;
-    }
-
-    public double getElectricalMetric()
-    {
-        return electrical;
+        return new StormData
+        {
+            flooding = this.flooding,
+            lightning = this.lightning,
+            wind = this.wind,
+            electrical = this.electrical,
+            direction = this.direction,
+            airPressure = this.airPressure,
+            stormName = this.stormName,
+            category = this.category
+        };
     }
 }
